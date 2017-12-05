0b24dac4961b673973ce16208a6232196400fc51
denzo <deniss.alimovs@gmail.com>
2017-12-05 18:10:40 +1100

    use async/await in acceptance tests

--- tests/acceptance/calendar-test.js
+++ tests/acceptance/calendar-test.js
@@ -4,65 +4,43 @@ import moment from 'moment';
 
 moduleForAcceptance('Acceptance | calendar');
 
-test('visiting unrecognised URLs should redirect to the current month', function(assert) {
+test('visiting unrecognised URLs should redirect to the current month', async function(assert) {
   const today = moment();
   const year = today.format('YYYY');
   const monthNumber = today.format('M');
 
-  visit('/calendar');
-  andThen(function() {
-    assert.equal(currentURL(), `/calendar/${year}/${monthNumber}`);
-  });
+  await visit('/calendar');
+  assert.equal(currentURL(), `/calendar/${year}/${monthNumber}`);
 
-  visit('/');
-  andThen(function() {
-    assert.equal(currentURL(), `/calendar/${year}/${monthNumber}`);
-  });
+  await visit('/');
+  assert.equal(currentURL(), `/calendar/${year}/${monthNumber}`);
 
-  visit('/karamba');
-  andThen(function() {
-    assert.equal(currentURL(), `/calendar/${year}/${monthNumber}`);
-  });
+  await visit('/karamba');
+  assert.equal(currentURL(), `/calendar/${year}/${monthNumber}`);
 });
 
-test('visiting with invalid date in the URL should gracefully redirect to the current month', function(assert) {
+test('visiting with invalid date in the URL should gracefully redirect to the current month', async function(assert) {
   const today = moment();
   const year = today.format('YYYY');
   const monthNumber = today.format('M');
 
-  visit('/calendar/2017/13');
-  andThen(function() {
-    assert.equal(currentURL(), `/calendar/${year}/${monthNumber}`);
-  });
+  await visit('/calendar/2017/13');
+  assert.equal(currentURL(), `/calendar/${year}/${monthNumber}`);
 
-  visit('/calendar/2017/0');
-  andThen(function() {
-    assert.equal(currentURL(), `/calendar/${year}/${monthNumber}`);
-  });
+  await visit('/calendar/2017/0');
+  assert.equal(currentURL(), `/calendar/${year}/${monthNumber}`);
 
-  visit('/calendar/2017/123');
-  andThen(function() {
-    assert.equal(currentURL(), `/calendar/${year}/${monthNumber}`);
-  });
+  await visit('/calendar/2017/123');
+  assert.equal(currentURL(), `/calendar/${year}/${monthNumber}`);
 
-  visit('/calendar/2017/');
-  andThen(function() {
-    assert.equal(currentURL(), `/calendar/${year}/${monthNumber}`);
-  });
+  await visit('/calendar/2017/');
+  assert.equal(currentURL(), `/calendar/${year}/${monthNumber}`);
 
-  visit('/calendar/20173/5');
-  andThen(function() {
-    assert.equal(currentURL(), `/calendar/${year}/${monthNumber}`);
-  });
+  await visit('/calendar/20173/5');
+  assert.equal(currentURL(), `/calendar/${year}/${monthNumber}`);
 });
 
-test('it should display the right month', function(assert) {
-  const today = moment();
-  const year = today.format('YYYY');
-  const monthNumber = today.format('M');
-
-  visit('/calendar/2017/4');
-  andThen(function() {
-    assert.equal(find('[data-test-displayed-month]').text().trim(), 'April 2017');
-  });
+test('it should display the right month', async function(assert) {
+  await visit('/calendar/2017/4');
+  assert.equal(find('[data-test-displayed-month]').text().trim(), 'April 2017');
 });
